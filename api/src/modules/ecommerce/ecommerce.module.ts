9e7f8ddde8c6634b8c1c17c7f0a52a19a0918eee
chessco <chesssco@gmail.com>
2026-05-12 17:55:46 -0700

    fix: resolve backend compilation and frontend reference errors

--- api/src/modules/ecommerce/ecommerce.module.ts
+++ api/src/modules/ecommerce/ecommerce.module.ts
@@ -1,3 +1,4 @@
+import { Module } from '@nestjs/common';
 import { EcommerceController } from './ecommerce.controller';
 import { StorefrontController } from './storefront.controller';
 import { EcommerceService } from './ecommerce.service';
